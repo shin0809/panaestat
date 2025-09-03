ca2b290a78f996de2aac651e8aa6e9705eeeb6e1
shin0809 <yuasa.shingo@jp.panasonic.com>
2025-09-04 02:37:38 +0900

    new llm 20250904

--- app/utils/generative_ai_model.py
+++ app/utils/generative_ai_model.py
@@ -5,9 +5,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_anthropic import ChatAnthropic
 
 class GenerativeAIModel(Enum):
-    GPT_4O = "gpt-4o"
-    GEMINI_PRO = "gemini-1.5-pro-latest"
-    CLAUDE_SONNET = "claude-3-5-sonnet-20240620"
+    GPT_4O = "gpt-5-nano"
+    GEMINI_PRO = "gemini-2.5-flash"
+    CLAUDE_SONNET = "claude-sonnet-4-20250514"
 
 def set_llm(llm_name):
     if llm_name == GenerativeAIModel.GPT_4O.value:
